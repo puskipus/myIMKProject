e8cac1363aec25b0695eb0501076e7d71013e83d
Muhammad Abyan Zafran <abyan@zafflen.xyz>
2021-11-23 16:54:11 +0700

    added clickable div to change locations in the locations page

--- LocationScript.js
+++ LocationScript.js
@@ -1,13 +1,26 @@
 /* buat mengubah url lokasi*/
 
 const locs = document.getElementsByClassName("location-click");
-const map = document.getElementById('gmap')
-let jogjaurl = JSON.parse("urlLokasi.json")
-map.src = jogjaurl
+const mapIfr = document.getElementById("gmap");
 
-for(let i =0; i< locs.length;i++){
-    console.log(locs[i])
-    locs[i].onclick = () =>{
-        console.log("hello from "+i)
-    }
-}
+const cabangs = (i) => {
+  switch (i) {
+    case 0:
+      return DATALOC.malang;
+    case 1:
+      return DATALOC.jogja;
+    case 2:
+      return DATALOC.jakarta;
+  }
+};
+
+const mnFunc = () => {
+  for (let i = 0; i < locs.length; i++) {
+    // console.log(locs[i]);
+    locs[i].onclick = () => {
+      mapIfr.src = cabangs(i);
+    };
+  }
+};
+
+mnFunc();
