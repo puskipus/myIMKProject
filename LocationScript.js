0b067b031f18ddd60d478a17c515d191a6b6a341
Muhammad Abyan Zafran <abyan@zafflen.xyz>
2021-11-23 19:56:15 +0700

    added highlighted color feature

--- LocationScript.js
+++ LocationScript.js
@@ -1,7 +1,14 @@
 /* buat mengubah url lokasi*/
 
+/* constant variables */
+
+const SEL_COLOR = "white";
+const UNSEL_COLOR = "grey";
+
 const locs = document.getElementsByClassName("location-click");
 const mapIfr = document.getElementById("gmap");
+
+
 
 const cabangs = (i) => {
   switch (i) {
@@ -14,13 +21,27 @@ const cabangs = (i) => {
   }
 };
 
-const mnFunc = () => {
+const handleColor = (locs, idx) => {
+  locs[idx].style.backgroundColor = SEL_COLOR;
+  for (const highlighted of locs) {
+    if (locs[idx] != highlighted) {
+      highlighted.style.backgroundColor = UNSEL_COLOR;
+    }
+  }
+};
+
+const assignClick = () => {
+  locs[0].style.backgroundColor = SEL_COLOR;
   for (let i = 0; i < locs.length; i++) {
-    // console.log(locs[i]);
     locs[i].onclick = () => {
       mapIfr.src = cabangs(i);
+      handleColor(locs,i);
     };
   }
+};
+
+const mnFunc = () => {
+  assignClick();
 };
 
 mnFunc();
