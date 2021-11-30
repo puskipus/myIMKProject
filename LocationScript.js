23c131fa2d378537e44a7351f2f4d3f724bfd403
Muhammad Abyan Zafran <abyan@zafflen.xyz>
2021-11-30 20:39:49 +0700

    added my styling

--- LocationScript.js
+++ LocationScript.js
@@ -10,46 +10,51 @@ const mapIfr = document.getElementById("gmap");
 
 const selValues = document.getElementById("list-lokasi");
 
+/* File path */
+const path = window.location.pathname;
+const currFile = path.split("/").pop();
 
 const assignSelect = () => {
-  selValues.onchange = () => {
-    mapIfr.src = cabangs(selValues.options.selectedIndex);
-  };
+    selValues.onchange = () => {
+        mapIfr.src = cabangs(selValues.options.selectedIndex);
+    };
 };
 
 const cabangs = (i) => {
-  switch (i) {
-    case 0:
-      return DATALOC.malang;
-    case 1:
-      return DATALOC.jogja;
-    case 2:
-      return DATALOC.jakarta;
-  }
+    switch (i) {
+        case 0:
+            return DATALOC.malang;
+        case 1:
+            return DATALOC.jogja;
+        case 2:
+            return DATALOC.jakarta;
+    }
 };
 
 const handleColor = (locs, idx) => {
-  locs[idx].style.backgroundColor = SEL_COLOR;
-  for (const highlighted of locs) {
-    if (locs[idx] != highlighted) {
-      highlighted.style.backgroundColor = UNSEL_COLOR;
+    locs[idx].style.backgroundColor = SEL_COLOR;
+    for (const highlighted of locs) {
+        if (locs[idx] != highlighted) {
+            highlighted.style.backgroundColor = UNSEL_COLOR;
+        }
     }
-  }
 };
 
 const assignClickSubpage = () => {
-  locs[0].style.backgroundColor = SEL_COLOR;
-  for (let i = 0; i < locs.length; i++) {
-    locs[i].onclick = () => {
-      mapIfr.src = cabangs(i);
-      handleColor(locs, i);
-    };
-  }
+    locs[0].style.backgroundColor = SEL_COLOR;
+    for (let i = 0; i < locs.length; i++) {
+        locs[i].onclick = () => {
+            mapIfr.src = cabangs(i);
+            handleColor(locs, i);
+        };
+    }
 };
 
 const mnFunc = () => {
-  assignSelect();
-  assignClickSubpage();
+    switch(currFile){
+        case "index.html" : assignSelect(); break;
+        case "lokasi.html" : assignClickSubpage(); break;
+    }
 };
 
 mnFunc();
